02164458e34d43daf2a987f1cc031bdf03d232c4
Jiwuuuu <andr.amrinto@gmail.com>
2026-04-24 19:45:35 +0800

    wip winning condition changes

--- gamedev.js-entry/Objects/Drop Off Area/drop_off_area.gd
+++ gamedev.js-entry/Objects/Drop Off Area/drop_off_area.gd
@@ -19,7 +19,11 @@ func _ready() -> void:
 
 func collect(item : Node2D) -> void:
 	if not item is Item: return
-	
+	if item.item_data != ScoreKeeper.target_item_data:
+		item.dropped_into_box = false
+		item.velocity = Vector2(0, -500)
+		return
+
 	collected_item_data.append(item.item_data)
 	collected.emit(item)
 	item.queue_free()
